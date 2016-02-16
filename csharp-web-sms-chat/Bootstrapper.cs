d53949d4f17f3cb2197ba4ca7edefb169432a3cc
Andrey Belchikov <belchikovs@gmail.com>
2016-02-16 14:48:34 +0300

    Fixed issue with static files

--- csharp-web-sms-chat/Bootstrapper.cs
+++ csharp-web-sms-chat/Bootstrapper.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Linq;
 using Nancy;
 using Nancy.Bootstrapper;
+using Nancy.Conventions;
 using Nancy.TinyIoc;
 
 namespace WebSmsChat
@@ -9,18 +11,29 @@ namespace WebSmsChat
   {
     protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
     {
+      var staticFiles = new[] {"/index.html", "/config.js", "/app/", "/styles/", "/node_modules/"};
       pipelines.BeforeRequest += c =>
       {
         //SPA support
-        if (c.Request.Method == "GET" && c.Request.Path.IndexOf("/Content/", StringComparison.Ordinal) < 0)
+        if (c.Request.Method == "GET" &&
+          !staticFiles.Any(path => c.Request.Path.IndexOf(path, StringComparison.Ordinal) >= 0))
         {
           var response = new Response {StatusCode = HttpStatusCode.MovedPermanently};
-          response.Headers.Add("Location", "/Content/index.html");
+          response.Headers.Add("Location", "/index.html");
           return response;
         }
         return null;
       };
       base.ApplicationStartup(container, pipelines);
+    }
+
+    protected override void ConfigureConventions(NancyConventions conventions)
+    {
+      base.ConfigureConventions(conventions);
+
+      conventions.StaticContentsConventions.Add(
+          StaticContentConventionBuilder.AddDirectory("", "Content")
+      );
     }
   }
 }
